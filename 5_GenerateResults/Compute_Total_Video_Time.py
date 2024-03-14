71d575e6252a92d94fd21de60e01e0965abe345a
Carl <hildebrandt.carl@outlook.com>
2024-03-14 12:35:00 -0400

    Added time computation

--- 5_GenerateResults/Compute_Total_Video_Time.py
+++ 5_GenerateResults/Compute_Total_Video_Time.py
@@ -3,6 +3,31 @@ import os
 import glob
 import argparse
 
+from tqdm import tqdm
+
+
+def get_video_length(video_path):
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+    
+    if not cap.isOpened():
+        print("Error: Could not open video.")
+        return None
+    
+    # Get the frame rate
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    
+    # Get the total number of frames
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    # Calculate the length of the video in seconds
+    length_seconds = total_frames / fps
+    
+    # Release the video capture object
+    cap.release()
+    
+    return length_seconds
+
 # Get the Data
 parser = argparse.ArgumentParser(description="Displays the number of failure inducing inputs")
 parser.add_argument('--dataset_directory',
@@ -18,3 +43,45 @@ DATASET_DIRECTORY = f"{args.dataset_directory}"
 available_datasets_paths = glob.glob(f"{DATASET_DIRECTORY}/*")
 available_datasets = [os.path.basename(dset) for dset in available_datasets_paths]
 available_datasets = sorted(available_datasets)
+
+all_dataset_time = 0
+
+# For each of the datasets
+for dataset in tqdm(available_datasets, desc="Processing Dataset", leave=False, position=0):
+
+    # Get all video files
+    video_files = sorted(glob.glob(f"{DATASET_DIRECTORY}/{dataset}/1_ProcessedData/*.mp4"))
+    
+
+    total_time = 0
+
+    # For each video
+    for video_file in tqdm(video_files, desc="Processing Video", leave=False, position=1):
+
+        # Open the video and determine how long it is
+        video_time = get_video_length(video_file)
+        # print(f"{os.path.basename(video_file)} is: {video_time} seconds")
+        total_time += video_time
+
+    # Print the length
+    print(f"\n\nProcessing Dataset: {dataset}")
+    print(f"Total Time in Seconds: {total_time}")
+
+    hours = int(total_time) // 3600
+    minutes = (int(total_time) % 3600) // 60
+    seconds = int(total_time) % 60
+
+    print(f"Total Time (HH:MM:SS): {hours:02d}:{minutes:02d}:{seconds:02d}\n\n")
+
+
+    all_dataset_time += total_time
+
+
+hours = int(all_dataset_time) // 3600
+minutes = (int(all_dataset_time) % 3600) // 60
+seconds = int(all_dataset_time) % 60
+
+print("\n\n============================")
+print(f"Total Time for all datasets in Seconds: {total_time}")
+print(f"Total Time for all datasets (HH:MM:SS): {hours:02d}:{minutes:02d}:{seconds:02d}\n\n")
+print("============================")
